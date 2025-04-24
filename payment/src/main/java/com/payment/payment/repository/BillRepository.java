a38409f88e582682046d5e7e5c6f0e7e6ae810f4
atharvapimple24 <atharvapimple24@gmail.com>
2025-04-24 17:27:05 +0530

    Initial commit on backend branch

--- payment/src/main/java/com/payment/payment/repository/BillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.payment.payment.repository;
-
-import com.payment.payment.model.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface BillRepository extends JpaRepository<Bill, Long> {
-    Optional<Bill> findByOrder_OrderId(Long orderId);
-}
